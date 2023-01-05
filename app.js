922dfe89a818bb5ad9755f077c7997a7c3fb5990
darkimpow <darkimpowell@gmail.com>
2023-01-05 12:05:49 -0500

    deleted previous file from app.js

--- app.js
+++ app.js
@@ -60,28 +60,4 @@ let water =  Math.floor(Math.random() * 100);
     document.getElementById('dion').style.setProperty('--value', dion);
 
 
-    // Randomize numbers 1-10 for supply page/ amount left section 
-
-    document.getElementById("new1").value =
-    Math.floor(Math.random() * 10);
-
-    document.getElementById("pos2").value =
-    Math.floor(Math.random() * 10);
-
-    document.getElementById("pos3").value =
-    Math.floor(Math.random() * 10);
-
-    document.getElementById("pos4").value =
-    Math.floor(Math.random() * 10);
-
-    document.getElementById("pos5").value =
-    Math.floor(Math.random() * 10);
-
-    document.getElementById("pos6").value =
-    Math.floor(Math.random() * 10);
-
-    document.getElementById("pos7").value =
-    Math.floor(Math.random() * 10);
-
-    document.getElementById("pos8").value =
-    Math.floor(Math.random() * 10)
+  
