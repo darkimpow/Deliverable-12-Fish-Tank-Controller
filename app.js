a9c2a6f813b72603da4b53258241a9d0670353a6
darkimpow <darkimpowell@gmail.com>
2023-01-05 12:01:17 -0500

    updated sensors & supply page with random number generators

---
+++ app.js
@@ -0,0 +1,87 @@
+
+
+
+  // fucntions for random generated between 1-100 for sensors of the aquarium
+
+let water =  Math.floor(Math.random() * 100);
+    document.getElementById('water').innerHTML= water;
+    document.getElementById('water').style.setProperty('--value', water);
+
+    let chlorine =  Math.floor(Math.random() * 100);
+    document.getElementById('chlorine').innerHTML= chlorine;
+    document.getElementById('chlorine').style.setProperty('--value', chlorine);
+
+    let ph =  Math.floor(Math.random() * 100);
+    document.getElementById('ph').innerHTML= ph;
+    document.getElementById('ph').style.setProperty('--value', ph);
+
+    let gravel =  Math.floor(Math.random() * 100);
+    document.getElementById('gravel').innerHTML= gravel;
+    document.getElementById('gravel').style.setProperty('--value', gravel);
+
+    let food =  Math.floor(Math.random() * 100);
+    document.getElementById('food').innerHTML= food;
+    document.getElementById('food').style.setProperty('--value', food);
+
+    let ammonia =  Math.floor(Math.random() * 100);
+    document.getElementById('ammonia').innerHTML= ammonia;
+    document.getElementById('ammonia').style.setProperty('--value', ammonia);
+
+    let supply =  Math.floor(Math.random() * 100);
+    document.getElementById('supply').innerHTML= supply;
+    document.getElementById('supply').style.setProperty('--value', supply);
+
+    let nitrate =  Math.floor(Math.random() * 100);
+    document.getElementById('nitrate').innerHTML= nitrate;
+    document.getElementById('nitrate').style.setProperty('--value', nitrate);
+
+    let phosphate =  Math.floor(Math.random() * 100);
+    document.getElementById('phosphate').innerHTML= phosphate;
+    document.getElementById('phosphate').style.setProperty('--value', phosphate);
+
+    let silicate =  Math.floor(Math.random() * 100);
+    document.getElementById('silicate').innerHTML= silicate;
+    document.getElementById('silicate').style.setProperty('--value', silicate);
+
+    let strong =  Math.floor(Math.random() * 100);
+    document.getElementById('strong').innerHTML= strong;
+    document.getElementById('strong').style.setProperty('--value', strong);
+
+    let reef =  Math.floor(Math.random() * 100);
+    document.getElementById('reef').innerHTML= reef;
+    document.getElementById('reef').style.setProperty('--value', reef);
+
+    let fro =  Math.floor(Math.random() * 100);
+    document.getElementById('fro').innerHTML= fro;
+    document.getElementById('fro').style.setProperty('--value', fro);
+
+    let dion =  Math.floor(Math.random() * 100);
+    document.getElementById('dion').innerHTML= dion;
+    document.getElementById('dion').style.setProperty('--value', dion);
+
+
+    // Randomize numbers 1-10 for supply page/ amount left section 
+
+    document.getElementById("new1").value =
+    Math.floor(Math.random() * 10);
+
+    document.getElementById("pos2").value =
+    Math.floor(Math.random() * 10);
+
+    document.getElementById("pos3").value =
+    Math.floor(Math.random() * 10);
+
+    document.getElementById("pos4").value =
+    Math.floor(Math.random() * 10);
+
+    document.getElementById("pos5").value =
+    Math.floor(Math.random() * 10);
+
+    document.getElementById("pos6").value =
+    Math.floor(Math.random() * 10);
+
+    document.getElementById("pos7").value =
+    Math.floor(Math.random() * 10);
+
+    document.getElementById("pos8").value =
+    Math.floor(Math.random() * 10)
