f6b41325a4fb2474f2de9abbcedfabafeecddc97
darkimpow <darkimpowell@gmail.com>
2023-01-05 12:04:09 -0500

    seperated supply & sensor randomized js functions

---
+++ supply.js
@@ -0,0 +1,25 @@
+// Randomize numbers 1-10 for supply page/ amount left section 
+
+document.getElementById("new1").value =
+Math.floor(Math.random() * 10);
+
+document.getElementById("pos2").value =
+Math.floor(Math.random() * 10);
+
+document.getElementById("pos3").value =
+Math.floor(Math.random() * 10);
+
+document.getElementById("pos4").value =
+Math.floor(Math.random() * 10);
+
+document.getElementById("pos5").value =
+Math.floor(Math.random() * 10);
+
+document.getElementById("pos6").value =
+Math.floor(Math.random() * 10);
+
+document.getElementById("pos7").value =
+Math.floor(Math.random() * 10);
+
+document.getElementById("pos8").value =
+Math.floor(Math.random() * 10)
